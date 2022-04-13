8bcea8da5fcb31ec97298a1896e9e9c178ab62e2
Vakulina <viktoria.ganina@yandex.ru>
2022-04-13 20:38:10 +0300

    init: start space-page

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  // подключите плагины к PostCSS
+  plugins: [
+    // подключите autoprefixer
+    autoprefixer,
+  ]
+}; 
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+const cssLoaders = extra => {
+  const loaders =  [MiniCssExtractPlugin.loader, {
+    loader: 'css-loader',
+    options: {
+      importLoaders: 1
+    }
+  },
+  'postcss-loader'
+]
+  if (extra) {
+    loaders.push(extra)
+  }
+
+  return loaders
+}
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    hot: true,
+    open: true // сайт будет открываться сам при запуске npm run dev
+  },
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      
+      
+      ]
+  },
+ // devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new HtmlWebpackPlugin({
+       template: 'src/index.html',
+       }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin() 
+]
+
+}
