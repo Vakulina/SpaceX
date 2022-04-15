ed00e5ca11e2ea132e68493476729da891a136d6
Vakulina <viktoria.ganina@yandex.ru>
2022-04-15 11:19:30 +0300

    fix: fix transform bg on touch-screen

--- src/index.js
+++ src/index.js
@@ -7,20 +7,32 @@ let line = document.querySelector('.parallax__line');
 let ship = document.querySelector('.parallax__ship');
 let isTransform = false;
 
-const movieCursor= (e)=>{
-   let x = e.clientX / window.innerWidth;
+const transform = (x,y)=>{
+  bg.style.transform = `translate( -${x * 15}px, -${y * 10}px)`;
+
+  if(isTransform === false) {
+   ship.style.transform = `scale(.7) rotateX(40deg)`;
+   line.style.transform = `translateY(-70px) skewY(-5deg)`;
+   isTransform= true}
+  }
+  
+
+const moveMouse= (e)=>{
+let x = e.clientX / window.innerWidth;
 let y = e.clientY / window.innerHeight;
-
-bg.style.transform = `translate( -${x * 15}px, -${y * 10}px)`;
-
-if(isTransform === false) {
- ship.style.transform = `scale(.7) rotateX(40deg)`;
- line.style.transform = `translateY(-70px) skewY(-5deg)`;
- isTransform= true}
+transform(x,y)
 }
+
+const moveTouch=(e)=>{
+
+ const x=e.changedTouches[0].pageX/window.innerWidth;
+  const y=e.changedTouches[0].pageY/ window.innerHeight;
+  transform(x,y)
+}
+
 window.addEventListener('touchmove', function (e) {
-  movieCursor(e)
+  moveTouch(e)
 });
 window.addEventListener('mousemove', function (e) {
-  movieCursor(e)
+  moveMouse(e);
 });
