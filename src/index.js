8297a3903452b36681c851d0914a4489c4bf5bfa
Vakulina <viktoria.ganina@yandex.ru>
2022-04-16 14:01:46 +0300

    feat: add function of toogle burger-button

--- src/index.js
+++ src/index.js
@@ -5,30 +5,63 @@ import './asset/pages/main/index.scss'
 let bg = document.querySelector('.parallax__bg');
 let line = document.querySelector('.parallax__line');
 let ship = document.querySelector('.parallax__ship');
+const burgerButton = document.getElementById('close-burger');
+const navList = document.querySelector('.header__list');
+
+//const buttonBurger=document.getElementById('close-burger');
 let isTransform = false;
+let isOpen=false;
+/*
+let timeoutId = null;
 
+const resizeListener = () => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => setWindowWidth(getWindth()), 150);
+};
 
-const transform = (x,y)=>{
-  bg.style.transform = `translate( -${x * 15}px, -${y * 10}px)`;
-
-  if((isTransform === false)&&(window.innerHeight < 2000)) {
-   ship.style.transform = `scale(.7) rotateX(40deg)`;
-   line.style.transform = `translateY(-70px) skewY(-5deg)`;
-   isTransform= true}
-  }
-  
-
-const moveMouse= (e)=>{
-let x = e.clientX / window.innerWidth;
-let y = e.clientY / window.innerHeight;
-transform(x,y)
+window.addEventListener('resize', resizeListener);
+*/
+function openBurger(e){
+  e.preventDefault();
+ isOpen=true;
+  burgerButton.classList.add('header__burger-button_toClose')
+}
+function closeBurger(e){
+ isOpen=false;
+  burgerButton.classList.remove('header__burger-button_toClose')
 }
 
-const moveTouch=(e)=>{
+function toogleBurger(e){
+e.preventDefault();
+!isOpen? openBurger(e) : closeBurger(e);
+}
 
- const x=e.changedTouches[0].pageX/window.innerWidth;
-  const y=e.changedTouches[0].pageY/ window.innerHeight;
-  transform(x,y)
+burgerButton.addEventListener('click', (e)=> {
+
+
+  toogleBurger(e)});
+
+const transform = (x, y) => {
+  bg.style.transform = `translate( -${x * 15}px, -${y * 10}px)`;
+
+  if ((isTransform === false) && (window.innerHeight < 2000) && (window.innerHeight > 800)) {
+    ship.style.transform = `scale(.7) rotateX(40deg)`;
+    line.style.transform = `translateY(-70px) skewY(-5deg)`;
+    isTransform = true;
+  }
+}
+
+
+const moveMouse = (e) => {
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+  transform(x, y)
+}
+
+const moveTouch = (e) => {
+  const x = e.changedTouches[0].pageX / window.innerWidth;
+  const y = e.changedTouches[0].pageY / window.innerHeight;
+  transform(x, y)
 }
 
 window.addEventListener('touchmove', function (e) {
